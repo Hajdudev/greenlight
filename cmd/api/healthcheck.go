8d91a9d5e5af246d76cf4f104b1b3e9b85eb5fb4
phls <hajdu2024@gmail.com>
2025-06-11 13:48:02 +0200

    sending json

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 // Declare a handler which writes a plain-text response with information about the
 // application status, operating environment and version.
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+	app.writeJSON(w, http.StatusOK, data, nil)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module greenlight.hajdu.dev
 
 go 1.24.2
 
-require github.com/julienschmidt/httprouter v1.3.0 // indirect
+require github.com/julienschmidt/httprouter v1.3.0
