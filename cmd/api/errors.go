5a3aacd5356a6af81fb1c7bc35b5564b27a2fbc7
phls <hajdu2024@gmail.com>
2025-06-13 12:33:12 +0200

    Fixed future bug that if two people update the item at the same time

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -5,11 +5,13 @@ import (
 	"net/http"
 )
 
-// The logError() method is a generic helper for logging an error message. Later in the
-// book we'll upgrade this to use structured logging, and record additional information
-// about the request including the HTTP method and URL.
 func (app *application) logError(r *http.Request, err error) {
 	app.logger.Println(err)
+}
+
+func (app *application) editConflictResponse(w http.ResponseWriter, r *http.Request) {
+	message := "unable to update the record due to an edit conflict, please try again"
+	app.errorResponse(w, r, http.StatusConflict, message)
 }
 
 func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
