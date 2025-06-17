97c657a108cf8bcbefa8a7d59ed71bf1dcb8caa9
phls <hajdu2024@gmail.com>
2025-06-17 13:55:56 +0200

    working on metadat

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,12 +1,20 @@
 package data
 
 import (
+	"math"
 	"slices"
 	"strings"
 
 	"greenlight.hajdu.dev/internal/validator"
 )
 
+type Metadata struct {
+	CurrentPage  int `json:"current_page,omitempty"`
+	PageSize     int `json:"page_size,omitempty"`
+	FirstPage    int `json:"first_page,omitempty"`
+	LastPage     int `json:"last_page,omitempty"`
+	TotalRecords int `json:"total_records,omitempty"`
+}
 type Filters struct {
 	Page         int
 	PageSize     int
@@ -20,6 +28,20 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.PageSize > 0, "page_size", "must be greater than zero")
 	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
 	v.Check(validator.In(f.Sort, f.SortSafelist...), "sort", "invalid sort value")
+}
+
+func calculateMetadata(totalRecords, page, pageSize int) Metadata {
+	if totalRecords == 0 {
+		// Note that we return an empty Metadata struct if there are no records.
+		return Metadata{}
+	}
+	return Metadata{
+		CurrentPage:  page,
+		PageSize:     pageSize,
+		FirstPage:    1,
+		LastPage:     int(math.Ceil(float64(totalRecords) / float64(pageSize))),
+		TotalRecords: totalRecords,
+	}
 }
 
 func (f Filters) limit() int {
